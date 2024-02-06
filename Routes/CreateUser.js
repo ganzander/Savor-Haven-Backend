176c68755a89860b84ee5fc1e29b1f56f022542f
ganzander <126795345+ganzander@users.noreply.github.com>
2024-02-06 15:37:41 +0530

    Update CreateUser.js

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -308,8 +308,8 @@ router.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "https://savor-haven.vercel.app/success",
+    cancel_url: "https://savor-haven.vercel.app/cancel",
   });
 
   res.send({ Success: "true", sessionId: session.id });
